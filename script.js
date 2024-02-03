7f9fb41d73ec4905813a2979247e15169d835366
Filipeelopess98 <filipee.lopess98@gmail.com>
2024-02-02 21:45:29 -0300

    02/02/2024

---
+++ script.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector(".menu");
+const navMenu = document.querySelector(".nav-menu");
+
+menu.addEventListener("click", () => {
+    menu.classList.toggle('ativo');
+    navMenu.classList.toggle('ativo');
+})
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
